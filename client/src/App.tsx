6b27a4294b898ced08d49ee51575649a62136f20
Avishai-Peretz <avishai651@gmail.com>
2022-08-23 08:44:53 +0300

    mobile responsive fixes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,16 +9,24 @@ import deloitteLogo from './assets/images/Deloitte_logo_white.png'
 const App = () => {
 
   return (
-    <div className='app-container column-fs-c'>  
-      <Link to='/' >
-        <img src={ deloitteLogo } alt='Deloitte' className='deloitte-logo' />
-      </Link>
-      <Routes>
-        <Route path='/' element={<Home/>} />  
-        <Route path='/search-results' element={<SearchResults/>} />  
-        <Route path='/admin' element={<Admin/>} />  
-      </Routes>
-    </div>
+    <>
+      <div className='app-container column-fs-c'>  
+        <Link to='/' >
+          <img src={ deloitteLogo } alt='Deloitte' className='deloitte-logo' />
+        </Link>
+        <Routes>
+          <Route path='/' element={<Home/>} />  
+          <Route path='/search-results' element={<SearchResults/>} />  
+          <Route path='/admin' element={<Admin/>} />  
+        </Routes>
+      </div>
+      <div className='app-container mobile column-fs-c'>  
+        <Link to='/' >
+          <img src={ deloitteLogo } alt='Deloitte' className='deloitte-logo' />
+        </Link>
+        <div className='rotate-img'></div>
+      </div>
+    </>
   )
 }
 
