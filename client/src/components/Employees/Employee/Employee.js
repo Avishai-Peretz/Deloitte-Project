91877ebf3802b9fc5dc1641dccc88ff4d3231a7e
Avishai-Peretz <avishai651@gmail.com>
2022-08-24 12:39:19 +0300

    search css effect

--- client/src/components/Employees/Employee/Employee.js
+++ client/src/components/Employees/Employee/Employee.js
@@ -21,38 +21,40 @@ const Employee = ({ employee, index, page, enterPress }) => {
       return { __html: inner };
     }   
   }
+   
+  const handleHover = (e) => { e.preventDefault(); dispatch({ type: 'SELECT_KEY', payload: index }) } 
   
-  const handleAutocomplete = async (e) => {
-    if (e) {
+  const handleClickOrEnter = async (e, searchField) => {
+    if (index === selectedIndex  && (enterPress||e)) {
+      if (e) {
         if (page === 'home') { navigate("/search-results", { replace: true }) }
         if (page === 'searchResults' && window.confirm('Are you sure you want to go to Admin(testing) page?')) {
           navigate("/admin", { replace: true })
         }
     }
     if (employee) {
-      dispatch({ type: 'ENTER', payload: index });
-      dispatch(setSearchValue({value:employee.Name, ID: employee._id}));
-      const getSearchTerms = [20, "Name"];
-      const searchObject = { searchValue: employee.Name, searchTerms: getSearchTerms };
-      dispatch(searchEmployees(searchObject)); 
+      if (searchField === 'Name') {
+        dispatch(setSearchValue({value:employee.Name, ID: employee._id}));
+        const getSearchTerms = [20, searchField];
+        const searchObject = { searchValue: employee.Name, searchTerms: getSearchTerms };
+        dispatch(searchEmployees(searchObject)); 
+      }
+      if (searchField === 'WorkTitle') {     
+        dispatch(setSearchValue({value: employee.WorkTitle, ID: employee._id}));
+        const getSearchTerms = [20, searchField];
+        const searchObject = { searchValue: employee.WorkTitle, searchTerms: getSearchTerms };
+        dispatch(searchEmployees(searchObject)); 
+      }
     }
-  }
-   
-  const handleHover = (e) => { e.preventDefault(); dispatch({ type: 'SELECT_KEY', payload: index }) } 
-  
-  const handleClickOrEnter = async (e) => {
-    if (index === selectedIndex  && (enterPress||e)) {
-      await handleAutocomplete(e);
-      if (page === 'home') { navigate("/search-results", { replace: true }) }
-      if (page === 'searchResults' && window.confirm('Are you sure you want to go to Admin(testing) page?')) {navigate("/admin", { replace: true })}
+    dispatch({ type: 'ENTER', payload: index });
   }} 
-  useEffect(() => { handleClickOrEnter() }, [enterPress])
+  useEffect(() => { handleClickOrEnter(null, searchField) }, [enterPress])
 
   
   return (
     <div
       className={`employee-container row-c-sb employee-${index} selected-${selectedIndex === index ? 'true' : 'false'}`}
-      onClick={handleClickOrEnter} onMouseEnter={handleHover} onMouseLeave={handleHover}
+      onClick={(e)=>handleClickOrEnter(e, getSearchField)} onMouseEnter={handleHover} onMouseLeave={handleHover}
       >
       <img className='employee-img' src={ employee.ImageUrl } />
       <div className='txt-container column-c-se'>
