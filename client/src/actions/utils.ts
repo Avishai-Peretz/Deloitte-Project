c313e0999624e8dd05dc4d09fe8e55b65b049abb
Avishai-Peretz <avishai651@gmail.com>
2022-09-04 03:05:17 +0300

    search sorting function .map instead of for loop

--- client/src/actions/utils.ts
+++ client/src/actions/utils.ts
@@ -1,17 +1,26 @@
 import { EmployeeData, Employees } from "../types";
 
 export const sortInputFirst = (input: string, data: Employees) => {
-    let sortPreparation = []
-    for (let i: number = 0; i < data.length; i++) {
-        const indexOfInputByName: number = data[i]['Name'].toLowerCase().indexOf(input.toLowerCase())
-        const indexOfInputByWorkTitle: number = data[i]['WorkTitle'].toLowerCase().indexOf(input.toLowerCase())
-        const indexedEmployee = { ...data[i], indexOfInputByName: indexOfInputByName, indexOfInputByWorkTitle:indexOfInputByWorkTitle  }
-        sortPreparation.push(indexedEmployee)
-    }
-    const sortByNameAndWorkTitle = sortPreparation.filter(employee => employee.indexOfInputByName >= 0 && employee.indexOfInputByWorkTitle >= 0 )
+    // let sortPreparation = []
+    
+    // for (let i: number = 0; i < data.length; i++) {
+    //     const indexOfInputByName: number = data[i]['Name'].toLowerCase().indexOf(input.toLowerCase())
+    //     const indexOfInputByWorkTitle: number = data[i]['WorkTitle'].toLowerCase().indexOf(input.toLowerCase())
+    //     const indexedEmployee = { ...data[i], indexOfInputByName: indexOfInputByName, indexOfInputByWorkTitle:indexOfInputByWorkTitle  }
+    //     sortPreparation.push(indexedEmployee)
+    // }
+    const sortPreparation = data.map((employee) => {
+        return {
+            ...employee,
+            indexOfInputByName: employee['Name'].toLowerCase().indexOf(input.toLowerCase()),
+            indexOfInputByWorkTitle: employee['WorkTitle'].toLowerCase().indexOf(input.toLowerCase())
+        }
+    })
+    const sortByNameAndWorkTitle = sortPreparation.filter(employee => employee.indexOfInputByName >= 0 && employee.indexOfInputByWorkTitle >= 0)
     const sortByName = sortPreparation.filter(employee => employee.indexOfInputByName >= 0 && employee.indexOfInputByWorkTitle < 0 )
     const sortByWorkTitle = sortPreparation.filter(employee => employee.indexOfInputByName < 0 && employee.indexOfInputByWorkTitle >= 0 )
-    sortByNameAndWorkTitle.sort((first: EmployeeData, sec:EmployeeData) => {
+    
+    sortByNameAndWorkTitle.sort((first: EmployeeData, sec: EmployeeData) => {
         const firstInputIndex : number = first.indexOfInputByName! + first.indexOfInputByWorkTitle!
         const secInputIndex : number = sec.indexOfInputByName! + sec.indexOfInputByWorkTitle!
         return firstInputIndex - secInputIndex
